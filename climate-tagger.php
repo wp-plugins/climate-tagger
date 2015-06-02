0356a3172a28a690cc95116bdf05827da9163f45
Aptivate <Aptivate@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 17:07:39 +0000

    v1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/climate-tagger/trunk@1172932 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ climate-tagger.php
@@ -0,0 +1,255 @@
+<?php
+/*
+  Plugin Name: Climate Tagger
+  Description: Recommends tags in a tag cloud based on reegle tagging API.
+  Version: 1.0.0
+  Author: Aptivate
+*/
+
+if ( is_dir( WPMU_PLUGIN_DIR . '/climate-tagger' ) ) {
+	define( 'CLIMATE_TAGGER_INCLUDES', WPMU_PLUGIN_URL . '/climate-tagger' );
+} else {
+	define( 'CLIMATE_TAGGER_INCLUDES', WP_PLUGIN_URL . '/climate-tagger' );
+}
+
+class ClimateTagger {
+
+	function admin_menu() {
+		add_options_page(
+			'Climate Tagger',
+			'Climate Tagger',
+			'manage_options',
+			'climate-tagger',
+			array( 'ClimateTagger', 'add_options_page_callback' ));
+
+	}
+
+	function admin_init()
+	{
+		self::set_defaults();
+
+		register_setting(
+			'climate_tagger_general_settings',
+			'climate_tagger_general_settings'
+		);
+	}
+
+	function set_defaults() {
+
+		$options = get_option( 'climate_tagger_general_settings' );
+
+		$options = wp_parse_args(
+			$options,
+			array(
+				'token' => '',
+				'limit' => '20',
+				'post_types' => 'post',
+			) );
+
+		update_option( 'climate_tagger_general_settings', $options );
+
+	}
+
+	function add_options_page_callback() {
+		?>
+		<div class="wrap">
+		<h2>Climate Tagger by Aptivate</h2>
+
+		<div>
+
+		<form method="post" action="options.php">
+
+<?php
+		settings_fields( 'climate_tagger_general_settings' );
+		$options = get_option( 'climate_tagger_general_settings' );
+
+		?>
+		<h3>General Settings</h3>
+
+		<table class="form-table">
+		<tr valign="top">
+		<th scope="row">Authentication token:</th>
+		<td>
+<?php
+		printf(
+			'<input type="text" id="climate-tagger-token" name="climate_tagger_general_settings[token]" value="%s" size="50" />',
+			esc_attr( $options['token'] )
+		);
+		echo '<br /><span class="description">A valid authentication token that has been generated in the reegle API dashboard. <a href="http://api.reegle.info/register/" target="_blank">http://api.reegle.info/register</a></span>';
+		?>
+		</td>
+		</tr>
+
+		<tr valign="top">
+		<th scope="row">Post types:</th>
+		<td>
+<?php
+		printf(
+			'<input type="text" id="climate-tagger-post-types" name="climate_tagger_general_settings[post_types]" value="%s" />',
+			esc_attr( $options['post_types'] )
+		);
+		echo '<br /><span class="description">Supported post types, separated by commas.</span>';
+		?>
+		</td>
+		</tr>
+
+		<tr valign="top">
+		<th scope="row">Maximum number of tags:</th>
+		<td>
+<?php
+		printf(
+			'<input type="text" id="climate-tagger-limit" name="climate_tagger_general_settings[limit]" value="%s" size="5" />',
+			esc_attr( $options['limit'] )
+		);
+		echo '<br /><span class="description">Maximum number of tags to retrieve from the reegle API and display in the word cloud.</span>';
+		?>
+		</td>
+		</tr>
+
+		</table>
+
+
+<?php
+		submit_button();
+		?>
+
+		</form>
+
+			  </div>
+
+											</div>
+<?php
+
+	}
+
+	function add_box() {
+		$options = get_option( 'climate_tagger_general_settings' );
+
+		$post_types = explode( ',', $options['post_types'] );
+
+		foreach ( $post_types as $post_type ) {
+			add_meta_box(
+				'boxid',
+				'Suggested Tags (reegle)',
+				array( ClimateTagger, 'box_routine' ),
+				trim( $post_type ),
+				'side',
+				'low');
+		}
+	}
+
+	function box_routine() {
+		$response = self::get_reegle_tagger_response();
+
+		if ( is_wp_error( $response ) ) {
+			echo $response->get_error_message();
+			return;
+		}
+
+		if ( $response['response']['code'] != 200 ) {
+			echo $response['body'];
+			return;
+		}
+
+		$tags_post = self::get_tags_from_response( $response );
+		if ( count( $tags_post ) == 0 ) {
+			echo "Click 'Save Draft' to refresh tag suggestions.";
+			return;
+		}
+
+		self::print_tag_cloud( $tags_post );
+	}
+
+	function print_tag_cloud( $tags_rec ) {
+		arsort( $tags_rec );
+
+		$min_size = 10;
+		$max_size = 24;
+
+		$minimum_strength = min( array_values( $tags_rec ) );
+		$maximum_strength = max( array_values( $tags_rec ) );
+
+		$spread = $maximum_strength - $minimum_strength;
+		if ( $spread == 0 ) {
+			$spread = 1;
+		}
+
+		$step = ( $max_size - $min_size ) / $spread;
+
+		foreach ( $tags_rec as $tag_name => $tag_strength ) {
+			$size = $min_size + ($tag_strength - $minimum_strength) * $step;
+				?>
+				<a href="#" style="font-size: <?php echo "$size"?>pt;" onClick="tag_add('<?php echo $tag_name; ?>');return false;"><?php echo "$tag_name"?></a>
+<?php
+		}
+
+		echo '&nbsp;&nbsp;&nbsp;';
+	}
+
+	function get_reegle_tagger_response() {
+		global $post;
+
+		$content = $post->post_title .  ' ' . $post->post_content;
+
+		$content = apply_filters(
+			'climate-tagger-content',
+			$content,
+			$post );
+
+		if ( is_wp_error( $content ) ) {
+			return $content;
+		}
+
+		// http://api.reegle.info/documentation
+
+		$url = 'http://api.reegle.info/service/extract';
+
+		$options = get_option( 'climate_tagger_general_settings' );
+
+		$language = apply_filters( 'climate-tagger-language', 'en', $post );
+
+		$fields = array(
+			'text' => $content,
+			'locale' => $language,
+			'format' => 'json',
+			'token' => $options['token'],
+			'countConcepts' => $options['limit'],
+		);
+
+		return wp_remote_post( $url, array( 'body' => $fields ) );
+	}
+
+	function get_tags_from_response( $response ) {
+		$result = json_decode( $response['body'], true );
+
+		$concepts = $result['concepts'];
+
+		$tags = array();
+
+		foreach ( $concepts as $concept ) {
+			$tags[ $concept['prefLabel'] ] = $concept['score'];
+		}
+
+		return $tags;
+	}
+
+	function admin_add_my_script()
+	{
+		wp_enqueue_script(
+			'climate-tagger-add-tag',
+			CLIMATE_TAGGER_INCLUDES . '/climate-tagger-add-tag.js',
+			array( 'jquery' )
+		);
+	}
+}
+
+if ( is_admin() ) {
+	add_action( 'admin_menu', array( 'ClimateTagger', 'admin_menu' ) );
+	add_action( 'admin_init', array( 'ClimateTagger', 'admin_init' ) );
+
+	add_action( 'admin_menu',
+				array( 'ClimateTagger', 'add_box' ) );
+	add_action( 'admin_print_scripts',
+				array( 'ClimateTagger', 'admin_add_my_script' ) );
+}
+?>
---
+++ tests/test-plugin.php
@@ -0,0 +1,317 @@
+<?php
+
+require_once 'climate-tagger.php';
+require_once 'mock-remote-post.php';
+require_once 'mock-option.php';
+
+require_once 'ClimateTaggerTestBase.php';
+
+class PluginTest extends ClimateTaggerTestBase {
+
+	public function test_can_be_created() {
+		$tagger = new ClimateTagger();
+
+		$this->assertThat( $tagger, $this->isInstanceOf( 'ClimateTagger' ) );
+	}
+
+	public function test_returns_content_if_filter_error() {
+		$tagger = new ClimateTagger();
+
+		add_filter(
+			'climate-tagger-content',
+			array( $this, 'return_error' ), 1, 2 );
+
+		$this->get_new_post();
+
+		$response = $tagger->get_reegle_tagger_response();
+
+		$this->assertThat( $response, $this->isInstanceOf( 'WP_Error' ) );
+	}
+
+	public function return_error() {
+		return new WP_Error();
+	}
+
+	public function test_text_includes_title() {
+		$tagger = new ClimateTagger();
+
+		$post = $this->get_new_post();
+		$post->post_title = 'FEATURE: Three Steps to Decarbonising Development for a Zero-Carbon Future';
+
+		$tagger->get_reegle_tagger_response();
+
+		global $_CLIMATE_TAGGER_MOCK_POST;
+
+		$text = $_CLIMATE_TAGGER_MOCK_POST['text'];
+
+		$this->assertThat(
+			$text,
+			$this->stringContains(
+				'FEATURE: Three Steps to Decarbonising Development for a Zero-Carbon Future'
+			)
+		);
+	}
+
+	public function test_text_includes_post_content() {
+		$tagger = new ClimateTagger();
+
+		$post = $this->get_new_post();
+
+		$content = <<<EOT
+A new World Bank report lays out three steps for a smooth transition to a zero-carbon future. Through data, examples and policy advice, it aims to help countries makes the shift. It tells us that to prevent temperatures from rising more than 2 degrees Celsius, the world will need to transform its energy uses and electricity from clean energy sources will play an important role.
+EOT;
+		$post->post_content = $content;
+
+		$tagger->get_reegle_tagger_response();
+
+		global $_CLIMATE_TAGGER_MOCK_POST;
+
+		$text = $_CLIMATE_TAGGER_MOCK_POST['text'];
+
+		$this->assertThat(
+			$text,
+			$this->stringContains( $content	)
+		);
+	}
+
+	public function test_by_default_language_is_english() {
+		$tagger = new ClimateTagger();
+
+		$this->get_new_post();
+
+		$tagger->get_reegle_tagger_response();
+
+		global $_CLIMATE_TAGGER_MOCK_POST;
+
+		$locale = $_CLIMATE_TAGGER_MOCK_POST['locale'];
+
+		$this->assertThat(
+			$locale,
+			$this->equalTo( 'en' )
+		);
+	}
+
+	public function test_language_filter_applied() {
+		$tagger = new ClimateTagger();
+
+		add_filter(
+			'climate-tagger-language',
+			array( $this, 'return_spanish' ), 1, 2 );
+
+		$this->get_new_post();
+
+		$tagger->get_reegle_tagger_response();
+
+		global $_CLIMATE_TAGGER_MOCK_POST;
+
+		$locale = $_CLIMATE_TAGGER_MOCK_POST['locale'];
+
+		$this->assertThat(
+			$locale,
+			$this->equalTo( 'es' )
+		);
+	}
+
+	public function return_spanish() {
+		return 'es';
+	}
+
+	public function test_format_is_json() {
+		$tagger = new ClimateTagger();
+
+		$this->get_new_post();
+
+		$tagger->get_reegle_tagger_response();
+
+		global $_CLIMATE_TAGGER_MOCK_POST;
+
+		$format = $_CLIMATE_TAGGER_MOCK_POST['format'];
+
+		$this->assertThat(
+			$format,
+			$this->equalTo( 'json' )
+		);
+	}
+
+	public function test_api_token_retrieved_from_options() {
+		$tagger = new ClimateTagger();
+
+		$this->set_option( 'token',
+			'dfkgjOoldsg3kKD6FSfkp7of9sjs8dofsdjosdfjA' );
+
+		$this->get_new_post();
+
+		$tagger->get_reegle_tagger_response();
+
+		global $_CLIMATE_TAGGER_MOCK_POST;
+
+		$token = $_CLIMATE_TAGGER_MOCK_POST['token'];
+
+		$this->assertThat(
+			$token,
+			$this->equalTo( 'dfkgjOoldsg3kKD6FSfkp7of9sjs8dofsdjosdfjA' )
+		);
+	}
+
+	public function test_word_limit_retrieved_from_options() {
+		$tagger = new ClimateTagger();
+
+		$this->set_option( 'limit',
+			'25' );
+
+		$this->get_new_post();
+
+		$tagger->get_reegle_tagger_response();
+
+		global $_CLIMATE_TAGGER_MOCK_POST;
+
+		$limit = $_CLIMATE_TAGGER_MOCK_POST['countConcepts'];
+
+		$this->assertThat(
+			$limit,
+			$this->equalTo( '25' )
+		);
+	}
+
+	public function test_api_url() {
+		$tagger = new ClimateTagger();
+
+		$this->get_new_post();
+
+		$tagger->get_reegle_tagger_response();
+
+		global $_CLIMATE_TAGGER_MOCK_URL;
+
+		$this->assertThat(
+			$_CLIMATE_TAGGER_MOCK_URL,
+			$this->equalTo( 'http://api.reegle.info/service/extract' )
+		);
+	}
+
+	public function test_tags_ordered_by_score() {
+		$tagger = new ClimateTagger();
+
+		$this->get_new_post();
+
+		global $_CLIMATE_TAGGER_MOCK_RESPONSE;
+
+		$_CLIMATE_TAGGER_MOCK_RESPONSE = array(
+			'body' => json_encode(array(
+				'concepts' => array(
+					array(
+						'prefLabel' => 'climate change',
+						'score' => 20,
+					),
+					array(
+						'prefLabel' => 'IPPC',
+						'score' => 1,
+					),
+					array(
+						'prefLabel' => 'energy',
+						'score' => 10,
+					),
+				))),
+			'response' => array(
+				'code' => 200,
+			)
+		);
+
+		ob_start();
+		$tagger->box_routine();
+		$output = ob_get_contents();
+		ob_end_clean();
+
+		$links = $this->get_html_elements_from_output( $output, 'a' );
+
+		$this->assertThat( (string)$links[0], $this->equalTo( 'climate change' ) );
+		$this->assertThat( (string)$links[1], $this->equalTo( 'energy' ) );
+		$this->assertThat( (string)$links[2], $this->equalTo( 'IPPC' ) );
+	}
+
+	public function test_error_returned_if_response_is_error() {
+		$tagger = new ClimateTagger();
+
+		$this->get_new_post();
+
+		global $_CLIMATE_TAGGER_MOCK_RESPONSE;
+
+		$error = new WP_Error();
+		$error->add( '1', 'An error occurred' );
+
+		$_CLIMATE_TAGGER_MOCK_RESPONSE = $error;
+
+		ob_start();
+		$tagger->box_routine();
+		$output = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertThat( $output, $this->equalTo( 'An error occurred' ) );
+	}
+
+	public function test_body_returned_for_non_200_status_code() {
+		$tagger = new ClimateTagger();
+
+		$this->get_new_post();
+
+		global $_CLIMATE_TAGGER_MOCK_RESPONSE;
+
+		$_CLIMATE_TAGGER_MOCK_RESPONSE = array(
+			'body' => 'Unrecognized API key',
+			'response' => array(
+				'code' => 403,
+			)
+		);
+
+		ob_start();
+		$tagger->box_routine();
+		$output = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertThat( $output, $this->equalTo( 'Unrecognized API key' ) );
+	}
+
+	public function test_user_prompted_to_save_draft_when_no_tags() {
+		$tagger = new ClimateTagger();
+
+		$this->get_new_post();
+
+		global $_CLIMATE_TAGGER_MOCK_RESPONSE;
+
+		$_CLIMATE_TAGGER_MOCK_RESPONSE = array(
+			'body' => json_encode(
+				array(
+					'concepts' => array(),
+				)
+			),
+			'response' => array(
+				'code' => 200,
+			)
+		);
+
+		ob_start();
+		$tagger->box_routine();
+		$output = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertThat(
+			$output,
+			$this->equalTo( "Click 'Save Draft' to refresh tag suggestions."
+			)
+		);
+	}
+
+	private function get_new_post() {
+		global $post;
+		$post = new StdClass();
+		$post->post_title = '';
+		$post->post_content = '';
+
+		return $post;
+	}
+
+	private function set_option( $name, $value ) {
+		global $_CLIMATE_TAGGER_MOCK_OPTIONS;
+
+		$_CLIMATE_TAGGER_MOCK_OPTIONS[ $name ] = $value;
+	}
+}
